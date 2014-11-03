f2221f2a4e9577008184e7578657e7fe44fb6698
Huy Le <huy.lenq@gmail.com>
2014-11-03 15:13:01 +0000

    Set up context

--- src/main/java/upload/Application.java
+++ src/main/java/upload/Application.java
@@ -1,5 +1,11 @@
 package upload;
 
+@Configuration
+@Co
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
