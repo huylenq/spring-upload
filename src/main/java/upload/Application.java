7816c312105fc6171327922147bc539dad7c7dc5
tran thanh duy <TranDuy@trans-MacBook-Pro-4.local>
2014-11-03 22:18:02 +0700

    Update

--- src/main/java/upload/Application.java
+++ src/main/java/upload/Application.java
@@ -1,7 +1,12 @@
 package upload;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
 @Configuration
-@Co
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
